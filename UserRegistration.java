36b8e9dc5f7937c15764ee58724706f4ced34061
JEEVARATHINAMR <jeevasmartpkr@gmail.com>
2022-05-12 19:15:08 +0530

    Added:Last_name_Validation_Function

--- UserRegistration.java
+++ UserRegistration.java
@@ -6,10 +6,18 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 	
-	private static String FirstNamepattern ="^[A-Z]{1}[a-z]+$" ;
+	private static String FirstNamepattern ="^[A-Z][a-z]{3,}$" ;
+	private static String LastNamepattern = "^[A-Z][a-z]{3,}$";
 	
 	public void FirstnameValidation(String FirstNamepattern, String name) {
 		Pattern p = Pattern.compile(FirstNamepattern);
+
+		Matcher m = p.matcher(name);
+		boolean result = m.matches();
+		System.out.println(result);
+	}
+	public void LastnameValidation(String LastNamepattern,String name) {
+		Pattern p = Pattern.compile(LastNamepattern);
 		
 		Matcher m = p.matcher(name);
 		boolean result = m.matches();
@@ -23,8 +31,12 @@ public class UserRegistration {
 		System.out.println("UserRegistration");
 		
 		System.out.println("Enter first name: ");
+		System.out.println("Enter Last name: ");
+	
 		String name = sc.next();
 	User.FirstnameValidation(FirstNamepattern,name);
+	
+	User.LastnameValidation(LastNamepattern,name);
 		
 	
 	}
