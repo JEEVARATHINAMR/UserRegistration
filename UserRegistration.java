51c3b1a8ff7dc2c9dd547d50551f3a9b57ed7c45
JEEVARATHINAMR <jeevasmartpkr@gmail.com>
2022-05-12 16:20:37 +0530

    Added:FirstNameValidationFunction

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,8 +1,32 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+	
+	private static String FirstNamepattern ="^[A-Z]{1}[a-z]+$" ;
+	
+	public void FirstnameValidation(String FirstNamepattern, String name) {
+		Pattern p = Pattern.compile(FirstNamepattern);
+		
+		Matcher m = p.matcher(name);
+		boolean result = m.matches();
+		System.out.println(result);
+	}
 	public static void main(String[] args) {
+		UserRegistration User = new UserRegistration();
+		
+		Scanner sc =new Scanner(System.in);
+		
 		System.out.println("UserRegistration");
+		
+		System.out.println("Enter first name: ");
+		String name = sc.next();
+	User.FirstnameValidation(FirstNamepattern,name);
+		
+	
 	}
 
 }
